b996afe14937159fd9549cbfe5fd326926389047
DavidTaapy <74765863+DavidTaapy@users.noreply.github.com>
2023-06-14 14:35:58 +0800

    Delete hidden.py

--- hidden.py
+++
@@ -1,5 +0,0 @@
-# Hidden Information
-APP_ID = 1115864292672163861
-PUB_KEY = "6e2bea5b822396aeaeb04a09c9cf5de1c40fbefcdd72a17b2bbfdc03152bfafe"
-URL = "https://discord.com/api/oauth2/authorize?client_id=1115864292672163861&permissions=1634235579456&scope=bot"
-TOKEN = "MTExNTg2NDI5MjY3MjE2Mzg2MQ.G4pBjN.Pz2YCm917rGcfZhDnqjf0gv2cW7vvxnGwWkaHo"
